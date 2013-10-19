39384e858fee53675e82cad0568be048374f9b24
SatoshiLiang <chihchengliang@gmail.com>
2013-10-19 13:23:25 +0800

    Initial dist subtree commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ayo5566App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      '那一年默默無言　只能選擇離開',
+      '無邪的笑容已經不再精彩',
+      '你害怕結局所以拚命傷害',
+      '說是我擋住你的美好未來'
+    ];
+  });
